4f04aa31b78b5f732f9077ee4cb5191efc089101
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2023-04-19 16:24:37 +0100

    Image compressed and inserted in a folder

--- submit.js
+++ submit.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-    var message = document.getElementsByClassName("message")[0];
-    var subscription = document.getElementsByClassName("subscribed")[0];
+    var message = document.getElementsByClassName("message")[0];  // targeting submit element
+    var subscription = document.getElementsByClassName("subscribed")[0]; //targeting subscribe element
   
     function showMessage() {
-      alert("The form is now submitted. We will contact you on this email soon!");
+      alert("The form is now submitted. We will contact you on this email soon!");   //Shows this alert when submit clicked
     }
 
     function showSubscribed() {
-        alert("Thanks for signing up to our mailing list..");
+        alert("Thanks for signing up to our mailing list..");   //Shows this alert when subscribe clicked
       }
   
-    message.addEventListener('click', showMessage, false);
-    subscription.addEventListener('click', showSubscribed, false);
+    message.addEventListener('click', showMessage, false);   // when submit is clicked
+    subscription.addEventListener('click', showSubscribed, false);  //when subscribe is clicked
   };
